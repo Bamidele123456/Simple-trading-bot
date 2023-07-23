747c696419f37327ac24707101955e8e740eae19
Bamidele123456 <bamideleprecious85@gmail.com>
2023-07-23 12:51:28 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,34 @@
+import alpaca_trade_api as tradeapi
+
+API_KEY = "PK4G3XX1Y9R5DBQ7QVDI"
+SECRET_KEY = "fdqmnZ8xImi2KhCzaUU9pZFohvORdwa8WmOMYcms"
+# API_KEY = "<Your API here>"
+# SECRET_KEY = "<Your secret key here>"
+
+#---------------- API INFO ----------------#
+api = tradeapi.REST(API_KEY,
+                    SECRET_KEY,
+                    'https://paper-api.alpaca.markets')
+
+#-------- Here basket of stocks ------------#
+symbols = ['AA', 'AAL', 'UAL', 'NIO', 'AMD', 'TSLA', 'BYND', 'NKLA', 'XPEV', 'NVDA']
+
+
+#---- NUMBER OF SHARES FOR EACH PURCHASE -----#
+loading = {'AA': 50,
+           'AAL': 50,
+           'UAL': 50,
+           'NIO': 50,
+           'AMD': 50,
+           'TSLA': 10,
+           'BYND': 20,
+           'NKLA': 30,
+           'XPEV': 30,
+           'NVDA': 50}
+
+#---- SET SLOW AND FAST MOVING AVERAGE -----#
+slow = 20
+fast = 1
+
+#------- FREQUENCY for you time interval -----------#
+freq = '1Min'
